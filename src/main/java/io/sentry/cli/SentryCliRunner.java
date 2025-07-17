26ab9ef8115aa6f162d87bd37b8076fe93e583fe
lcian <lorenzo.cian@sentry.io>
2025-07-17 11:27:56 +0200

    improve

--- src/main/java/io/sentry/cli/SentryCliRunner.java
+++ src/main/java/io/sentry/cli/SentryCliRunner.java
@@ -79,9 +79,9 @@ public class SentryCliRunner {
 
       return collectAndMaybePrintOutput(logFile, debugSentryCli);
     } catch (MojoExecutionException e) {
+      logger.error("Error while attempting to run Sentry CLI: ", e);
       if (logFile != null) {
         final @Nullable String output = collectAndMaybePrintOutput(logFile, true);
-        logger.error("Error while attempting to run Sentry CLI: ", e);
         if (output != null) {
           final @NotNull CliFailureReason failureReason = failureReasonFromCliOutput(output);
           throw new SentryCliException(failureReason);
